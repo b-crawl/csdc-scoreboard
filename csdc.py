9825ac706519986bdb0d2fce0daeb19e20a282ba
Edgar A. Bering IV <trizor@gmail.com>
2018-09-09 01:08:22 -0400

    track mibe since 8/1 as stress test

--- csdc.py
+++ csdc.py
@@ -259,6 +259,14 @@ def initialize_weeks():
                 end = datetime.datetime(2018,9,11),
                 bonus1 = lairbonus,
                 bonus2 = alllairbonus))
+        weeks.append(CsdcWeek(
+            number = "mibe",
+            species = "Mi",
+            background = "Be",
+            gods = ("Trog",),
+            start = datetime.datetime(2018,8,1),
+            end = datetime.datetime(2018,9,11)))
+
 
 def score(wk):
     titlestr = "CSDC Week {} {}{}".format(wk.number, wk.species.short,
